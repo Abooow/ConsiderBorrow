7fc20e05b3c314bf327de35fc7291bf5f0617fe4
Daniel Abdulahad <daniel00.abdulahad@gmail.com>
2023-05-08 19:47:15 +0200

    Added initial migration

---
+++ src/Server/Migrations/20230508174035_InitialCreate.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsiderBorrow.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "ResourceManagement");
+
+            migrationBuilder.EnsureSchema(
+                name: "Identity");
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                schema: "ResourceManagement",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                schema: "Identity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Salary = table.Column<decimal>(type: "money", nullable: false),
+                    IsCEO = table.Column<bool>(type: "bit", nullable: false),
+                    IsManager = table.Column<bool>(type: "bit", nullable: false),
+                    ManagerId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_Employees_ManagerId",
+                        column: x => x.ManagerId,
+                        principalSchema: "Identity",
+                        principalTable: "Employees",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LibraryItems",
+                schema: "ResourceManagement",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Pages = table.Column<int>(type: "int", nullable: true),
+                    RunTimeMinutes = table.Column<int>(type: "int", nullable: true),
+                    IsBorrowable = table.Column<bool>(type: "bit", nullable: false),
+                    Borrower = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    BorrowDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LibraryItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LibraryItems_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalSchema: "ResourceManagement",
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_CategoryName",
+                schema: "ResourceManagement",
+                table: "Categories",
+                column: "CategoryName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_ManagerId",
+                schema: "Identity",
+                table: "Employees",
+                column: "ManagerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LibraryItems_CategoryId",
+                schema: "ResourceManagement",
+                table: "LibraryItems",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees",
+                schema: "Identity");
+
+            migrationBuilder.DropTable(
+                name: "LibraryItems",
+                schema: "ResourceManagement");
+
+            migrationBuilder.DropTable(
+                name: "Categories",
+                schema: "ResourceManagement");
+        }
+    }
+}
