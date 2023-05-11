ee208036d7b23a62fbb6f49f1a306a9bbf5128ec
Daniel Abdulahad <daniel00.abdulahad@gmail.com>
2023-05-11 21:23:40 +0200

    Added DefaultImageProvider

---
+++ src/Client/DefaultImageProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsiderBorrow.Client;
+
+internal static class DefaultImageProvider
+{
+    public static string GetDefaultImage(string type)
+    {
+        return type switch
+        {
+            "DVD" => "images/no-image-circle.png",
+            "Audio Book" => "images/no-image-square.png",
+            _ => "images/no-image-rectangle.png"
+        };
+    }
+}
